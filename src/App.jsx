6b400775e6ac958baf74f049fe2e5f24d2c43179
Alper Yazagan <alperyazagan@gmail.com>
2023-06-22 23:22:50 +0300

    firebase working

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   onSnapshot,
   updateDoc,
   doc,
+  addDoc,
+  deleteDoc,
 } from "firebase/firestore";
 
 const style = {
@@ -22,8 +24,21 @@ const style = {
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
   //create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === "") {
+      alert("Please enter a valid todo");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      completed: false,
+    });
+    setInput("");
+  };
 
   //read todo from firebase
   useEffect(() => {
@@ -45,23 +60,39 @@ function App() {
     });
   };
   //delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form}>
-          <input type="text" placeholder="Add Todo"></input>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type="text"
+            placeholder="Add Todo"
+          ></input>
           <button className={style.button}>
             <AiOutlinePlus size={30} />
           </button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
           ))}
         </ul>
-        <p className={style.count}>You have {todos.length} todos</p>
+        {todos.length < 1 ? null : (
+          <p className={style.count}>You have {todos.length} todos</p>
+        )}
       </div>
     </div>
   );
